7391d4ca0b0eb24ac5a9760b03de90e40bbfb947
Adrian Farnell <farnelaj@legal.regn.net>
2018-12-11 09:36:28 +0000

    Added in 3 servers

--- azuredeploy.unit.tests.ps1
+++ azuredeploy.unit.tests.ps1
@@ -29,6 +29,7 @@ Describe "vfRegression Deployment Tests" {
     $output = Test-AzureRmResourceGroupDeployment `
               -ResourceGroupName $ResourceGroupName `
               -TemplateFile $TemplateFileLocation `
+              -TemplateParameterFile "$scriptPath\parameters.json" `
               -ErrorAction Stop `
                5>&1
     $result = (($output[32] -split "Body:")[1] | ConvertFrom-Json).properties
@@ -55,6 +56,43 @@ Describe "vfRegression Deployment Tests" {
     It "Should be deployed successfully" {
       $result.provisioningState | Should -Be "Succeeded"
     }
+
+    $arrayOfServers = "lnvfregsql01", "lnvfregsql02", "lnvfregsql03", "lnvfregsql04"
+
+
+    #generic server tests
+
+    # It "lnvfregrds01 should exist"
+    # {}
+
+    # It "lnvfregrds01 should be located in westeurope"
+    # {}
+    
+    # It "lnvregrds01 should report into main storage account"
+    # {}
+
+    # It "lnvregrds01 should have a NIC"
+    # {}
+
+    # It "lnvregrds01 should be added to correct adSubnet"
+    # {}
+
+    # It "lnvregrds01 should be added to correct vNET"
+    # {}
+
+    # It "lnvregrds01 should attempt to Join Domain"
+    # {}
+
+    # It "lnvregrds01 should be a 2016 server"
+    # {}
+
+    # It "lnvregrds01 should run InstallStandardApps"
+    # {}
+
+    # It "lnvregrds01 should auto shutdown"
+    # {}
+
+    #specific server tests
 
    }
 }
